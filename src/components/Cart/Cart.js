c1928973b2a6dc84000a340112c0a14d967b472d
Erika Green <erika.green@sureswiftcapital.com>
2022-09-20 09:51:21 -0400

    portal added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "c1",
+          name: "sushi",
+          amount: 2,
+          price: 12.99,
+        },
+      ].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+      </div>
+      <span>35.62</span>
+      <div className={classes.action}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
